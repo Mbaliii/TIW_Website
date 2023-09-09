7765e64f162d86b7c7f1c1da2b7994237e772f36
Mbali <mbalikhumalo408@gmail.com>
2023-09-09 19:32:41 +0200

    thread

--- thread/src/components/Header/Header.js
+++ thread/src/components/Header/Header.js
@@ -1,19 +1,23 @@
 import React from "react";
-import './Header.css'
+import './Header.css';
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
     return (
-        <div className="header" id='header'>
+        <div className="header flex flex-col" id='header'>
             {/* navbar */}
+
+            <Navbar />
 
             <div className="container flex">
                 <div className="header-content">
-                    <h2 className="text-uppercase text-white op-07 fw-6 ls-2">Fashion Design, Branding,</h2>
+                    <h2 className="text-uppercase text-white op-07 fw-6 ls-2">Fashion Designer, Branding,</h2>
                     <h1 className="text-white fw-6 header-title"> Hello, I'm a <span className="text-black">
                         Talented Fashion Designer In Diepkloof</span> that creates stunning and unique clothing <span className="text-black">Designs  </span>
                         inspired by the vibrant culture of the <span className="text-black">neighborhood</span></h1>
                     <div className="btn-groups flex">
                         <button type="button" className="btn-item bg-brown fw-4 ls-2">Explore My Work</button>
+                        <button type="button" className="btn-item bg-dark fw-4 ls-2">Contact Me</button>
                     </div>
                 </div>
             </div>
@@ -22,6 +26,3 @@ const Header = () => {
 }
 
 export default Header
-
-// "The talented fashion designer in Diepkloof creates stunning and unique 
-// clothing designs inspired by the vibrant culture of the neighborhood."
