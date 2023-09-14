5cc345757b2120e11e26e1f0290a2d59a243126f
Mbali <mbalikhumalo408@gmail.com>
2023-09-14 10:09:59 +0200

    thread

--- thread/src/App.js
+++ thread/src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
-import Contact from "./components/contact/Contact";
-import Experience from './components/experience/Experience';
-import Footer from "./components/footer/Footer";
 import Header from './components/header/Header';
 import Navbar from "./components/navbar/Navbar";
-import Portfolio from './components/portfolio/Portfolio';
+import About from "./components/about/About";
+import Experience from './components/experience/Experience';
 import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
     return (
         <>
-            <Header />
-            <Navbar/>
-            <Experience />
-            <Services />
-            <Portfolio />
-            <Testimonials />
-            <Contact />
-            <Footer />
+        <Header/>
+        <Navbar/>
+        <About/>
+        <Experience/>
+        <Services/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
         </>
     );
 };
--- thread/src/components/navbar/Navbar.js
+++ thread/src/components/navbar/Navbar.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import './navbar.css';
+import { BiSolidHomeAlt2} from 'react-icons/bi'
+import { FaUserAlt } from "react-icons/fa";
+import { FaBookOpen } from 'react-icons/fa';
+import { RiCustomerServiceLine } from 'react-icons/ri';
+import { BiSolidMessageSquareDetail } from "react-icons/bi";
+
+
 
 const Navbar = () => {
+    const [activeNav, setActiveNav] = useState('');
     return (
-        <div>Navbar</div>
+        <nav>
+            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiSolidHomeAlt2 /></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt /></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} ><FaBookOpen /></a>
+            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiCustomerServiceLine /></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiSolidMessageSquareDetail /></a>
+        </nav>
     )
 };
 
