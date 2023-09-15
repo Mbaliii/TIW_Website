a4caaf613359b1e71f3b2e0bb13209c2790529bb
Mbali <mbalikhumalo408@gmail.com>
2023-09-15 09:43:34 +0200

    thread

--- thread/src/components/experience/Experience.js
+++ thread/src/components/experience/Experience.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './experience.css';
-import { BsFillBagCheckFill } from 'react-icons/bs';
+import { BiSolidBadgeCheck } from 'react-icons/bi';
 
 const Experience = () => {
     return (
@@ -14,43 +14,101 @@ const Experience = () => {
 
                     <div className="experience__content">
                         <article className="experience__details">
-                            <BsFillBagCheckFill />
-                            <h4 className="blue-text">HTML</h4>
-                            <small className="text-light">Experienced</small>
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">HTML</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
                         </article>
 
                         <article className="experience__details">
-                            <BsFillBagCheckFill />
-                            <h4 className="blue-text">JavaScript</h4>
-                            <small className="text-light">Experienced</small>
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">JavaScript</h4>
+                                <small className="text-light">Intermediate</small>
+                            </div>
                         </article>
 
                         <article className="experience__details">
-                            <BsFillBagCheckFill />
-                            <h4 className="blue-text">HTML</h4>
-                            <small className="text-light">Experienced</small>
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">CSS</h4>
+                                <small className="text-light">Intermediate</small>
+                            </div>
                         </article>
 
                         <article className="experience__details">
-                            <BsFillBagCheckFill />
-                            <h4 className="blue-text">HTML</h4>
-                            <small className="text-light">Experienced</small>
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">Bootstrap</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
                         </article>
 
                         <article className="experience__details">
-                            <BsFillBagCheckFill />
-                            <h4 className="blue-text">HTML</h4>
-                            <small className="text-light">Experienced</small>
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">Tailwind</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
                         </article>
 
                         <article className="experience__details">
-                            <BsFillBagCheckFill />
-                            <h4 className="blue-text">HTML</h4>
-                            <small className="text-light">Experienced</small>
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">React</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
                         </article>
                     </div>
                 </div>
+                {/*End Of frontend */}
+
+
                 <div className="experience__backend">
+                    <h3 >BackEnd Development</h3>
+
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">Node</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">MongoDB</h4>
+                                <small className="text-light">Intermediate</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">PHP</h4>
+                                <small className="text-light">Intermediate</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">MySQL</h4>
+                                <small className="text-light">Basic</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BiSolidBadgeCheck className="experience__details-icon" />
+                            <div>
+                                <h4 className="blue-text">Python</h4>
+                                <small className="text-light">Basic</small>
+                            </div>
+                        </article>
+                    </div>
                 </div>
             </div>
         </section>
